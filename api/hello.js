693531800916cfc0fc9a593de04908d386ec780e
smilevent <1028330144@qq.com>
2022-01-07 19:13:40 +0800

    test

--- api/hello.js
+++ api/hello.js
@@ -1,4 +1,8 @@
+import got from "got";
 export default function handler(req, res) {
-    const { x,y,z } = req.query;
-    res.status(200).send(`x: ${x} y:${y} z:${z} !`);
+    const url = "https://mt0.google.com/vt/lyrs=s&x=0&y=0&z=0";
+    const { x, y, z } = req.query;
+    const res = await got(url, {}).buffer();
+    // res.status(200).send(`x: ${x} y:${y} z:${z} !`);
+    res.status(200).send(res);
 }
