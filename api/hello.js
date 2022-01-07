d8848d00229b68a0989ec7635f6a7897e267c038
liuxsdev <1028330144@qq.com>
2022-01-07 18:56:10 +0800

    test

--- api/hello.js
+++ api/hello.js
@@ -1,4 +1,4 @@
 export default function handler(req, res) {
-    const { name } = req.query;
-    res.status(200).send(`Hello ${name}!`);
+    const { x,y,z } = req.query;
+    res.status(200).send(`x: ${x} y:${y} z:${z} !`);
 }
